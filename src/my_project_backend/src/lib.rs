f45121427830d3501c52824ef35ea5f146822fde
Milo Milek <milomilekyes@gmail.com>
2024-12-03 17:51:39 +0000

    Full validation

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -13,11 +13,20 @@ fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, S
     if title.len()>250{
         return Err("Title is too long!".to_string())
     }
+    if content.len() > 500{
+        return Err("Content is too long!".to_string())
+    }
+    if tags.len() > 3{
+        return Err("Too many tags!".to_string())
+    }
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
     let last_blog = BLOGS.with(|blogs| blogs.borrow().last().expect("Vec should not be empty").clone());
     Ok(last_blog)
 }
+
+// content <= 500 znaków
+// tagi <= 3 tagi
 
 #[ic_cdk::query]
 fn get_blogs() -> Vec<Blog> {
